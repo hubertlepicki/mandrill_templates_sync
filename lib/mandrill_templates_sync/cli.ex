9a8113b4d2cca5e9c8f26cec5e14d05319550440
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2016-04-06 13:08:00 +0300

    Show how to use the tool

--- lib/mandrill_templates_sync/cli.ex
+++ lib/mandrill_templates_sync/cli.ex
@@ -21,7 +21,17 @@ defmodule MandrillTemplatesSync.Cli do
   end
 
   def process(:help) do
-    IO.puts "Usage: mandrill_templates_sync --source-key=KEY1 --destination-key=KEY2"
+    IO.puts """
+    Usage: mandrill_template_sync --souce-key=<KEY1> --destination_key=<KEY2>
+       or: mandrill_template_sync --account-key=<KEY> --from-env=<ENV1> --to-env=<ENV2>
+
+    Examples:
+      Copy all templates from source to destination account:
+        mandrill_templates_sync --source-key=KEY1 --destination-key=KEY2
+
+      Duplicate templates postfixed by "[staging]" and change postfix to "[production]" within one account:
+        mandrill_templates_sync --account-key=KEY --from-env=staging --to-env=production
+    """
   end
 
   def process([source_key, destination_key]) do
